14e01b3ea1c981abbff42b822dd4835c3bc9eb6b
Texasjake95 <texasjake95@gmail.com>
2019-09-10 12:22:28 -0500

    Fix input for movement

--- troll.gd
+++ troll.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_up"):
 		motion += Vector2(0, -1)
-	if Input.is_action_pressed("move_bottom"):
+	if Input.is_action_pressed("move_down"):
 		motion += Vector2(0, 1)
 	if Input.is_action_pressed("move_left"):
 		motion += Vector2(-1, 0)
