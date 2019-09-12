c48c6e1f16cb43a473f749d4dd03832ca1369df2
Texasjake95 <texasjake95@gmail.com>
2019-09-12 16:42:55 -0500

    Changed how collision works, it now looks for valid types before deciding what to do This allows different function to be called based on inheritance (see Item and Item2 collisions)

--- collision_manager.gd
+++ collision_manager.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 const collisionMappping = Dictionary()
 
+#TODO create a TileMap version of this script that this script will call in the event there is a TileMap involved
 func addMapping(clazz1, clazz2, function : String):
 
 	var key = [clazz1, clazz2]
-	key.sort()
 	
 	print(key)
 	
@@ -23,21 +23,39 @@ func handleCollision(entity1, entity2, collisionData : KinematicCollision2D):
 	if not entity1 || not entity2:
 		return
 	
-	var key = [getType(entity1), getType(entity2)]
-	key.sort()
+	var functionName = null
 	
-	if not collisionMappping.has(key):
+	for key in collisionMappping.keys():
+		
+		var entity1Valid = false
+		var entity2Valid = false
+		
+		for type in key:
+			print(type)
+			
+			if entity1 is type:
+				entity1Valid = true
+			elif entity2 is type:
+				entity2Valid = true	
+		
+		if entity1Valid and entity2Valid:
+			functionName = collisionMappping.get(key)
+			break
+	
+	if not functionName:
+		var key = [getType(entity1), getType(entity2)]	
+		key.sort()
+		
 		print("NOT FOUND:")
 		print(key)
 		return
 	
-	var functionName = collisionMappping.get(key)
-	
 	call(functionName, entity1, entity2, collisionData)
 
-func _ready():
-	addMapping(Item, Player, "playerItemHandle")
+func _ready():	
 	addMapping(Item2, Player, "playerItemHandle2")
+	addMapping(Item, Player, "playerItemHandle")
+
 
 func getFromCollision(collisionData : KinematicCollision2D):
 	var node = collisionData.collider
@@ -63,19 +81,19 @@ func getType(node):
 	
 	ret = typeof(node)
 	return ret
-
-
+	
 func playerItemHandle2(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
 
 	if not entity1 is Player || not entity2 is Item2:
 		return
-	
+
 	var item : Item2 = entity2
 	var player : Player = entity1
 	
-	print("Called from other handle")
-	item.remove()
+	print("NEW HANDLE")
 	
+	item.remove()
+
 func playerItemHandle(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
 
 	if not entity1 is Player || not entity2 is Item:
