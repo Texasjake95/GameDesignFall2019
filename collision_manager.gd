f5a3e4aab80ab601cdf54acd8d59f5181e9c47b0
Texasjake95 <texasjake95@gmail.com>
2019-09-12 23:22:18 -0500

    Add an underscore for "private" functions Use a more reliable way to check is a node still exists

--- collision_manager.gd
+++ collision_manager.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const collisionMappping = Dictionary()
 
-func addMapping(clazz1, clazz2, function : String):
+func _addMapping(clazz1, clazz2, function : String):
 
 	var key = [clazz1, clazz2]
 	
@@ -13,24 +13,24 @@ func moveAndCollide(entity : Entity_Base, velocity : Vector2, delta):
 	
 	var collision_info = entity.move_and_collide(velocity * delta)
 	if collision_info:
-		handleCollision(entity, collision_info.collider, collision_info)
+		_handleCollision(entity, collision_info.collider, collision_info)
 		
 func moveAndSlide(entity : Entity_Base, velocity : Vector2):
 
 	entity.move_and_slide(velocity)
 	for i in entity.get_slide_count():
 		var collision_info = entity.get_slide_collision(i)
-		handleCollision(entity, collision_info.collider, collision_info)
+		_handleCollision(entity, collision_info.collider, collision_info)
 
 		
-func handleCollision(entity1, entity2, collisionData : KinematicCollision2D):
+func _handleCollision(entity1, entity2, collisionData : KinematicCollision2D):
 	
 	if not entity1 || not entity2:
 		return
 	
 	# Sometimes moveAndSlide does the same node several times if it was removed
 	# at any point do not interact again
-	if entity1.is_queued_for_deletion() || entity2.is_queued_for_deletion():
+	if not is_instance_valid(entity1) || not is_instance_valid(entity2):
 		return
 	
 	var functionName = null
@@ -57,9 +57,9 @@ func handleCollision(entity1, entity2, collisionData : KinematicCollision2D):
 	call(functionName, entity1, entity2, collisionData)
 
 func _ready():	
-	addMapping(TileMap, Player, "playerTileHandle")
-	addMapping(Item2, Player, "playerItemHandle2")
-	addMapping(Item, Player, "playerItemHandle")
+	_addMapping(TileMap, Player, "_playerTileHandle")
+	_addMapping(Item2, Player, "_playerItemHandle2")
+	_addMapping(Item, Player, "_playerItemHandle")
 
 func getType(node):
 		
@@ -73,12 +73,12 @@ func getType(node):
 	ret = typeof(node)
 	return ret
 
-func playerTileHandle(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
+func _playerTileHandle(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
 	#TODO call an external script to handle TileMap Entity interaction
 	print("TILE HANDLED")
 	
 
-func playerItemHandle2(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
+func _playerItemHandle2(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
 
 	if not entity1 is Player || not entity2 is Item2:
 		return
@@ -90,7 +90,7 @@ func playerItemHandle2(entity1 : Entity_Base, entity2 : Entity_Base, collisionDa
 	
 	item.remove()
 
-func playerItemHandle(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
+func _playerItemHandle(entity1 : Entity_Base, entity2 : Entity_Base, collisionData : KinematicCollision2D):
 
 	if not entity1 is Player || not entity2 is Item:
 		return
