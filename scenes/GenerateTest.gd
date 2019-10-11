dc31bce3b8d5cbe9e77446dd25adff295330356b
Texasjake95 <texasjake95@gmail.com>
2019-10-11 15:38:58 -0500

    Reset json back to default and remove debug

--- scenes/GenerateTest.gd
+++ scenes/GenerateTest.gd
@@ -15,13 +15,4 @@ func _ready():
 	generated.set_tileset(tile_set)
 	generated.set_cell_size(Vector2(30,30))
 	
-	var array = util.WeightedArray.new()
-	array.add("1", 1)
-	array.add("2", 2)
-	array.add("3", 3)
-	array.add("4", 4)
-	
-	for i in array:
-		print(i)
-	
 	add_child(generated)
